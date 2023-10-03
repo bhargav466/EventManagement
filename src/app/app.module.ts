73a241a96e33dac835ba0a37ba6edfbef67dfb15
bhargav466 <mvsbhargav466@gmail.com>
2023-10-03 10:26:40 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileEditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
